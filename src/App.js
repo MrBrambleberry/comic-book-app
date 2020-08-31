501650e49359b00fa865d8693ad98fe36ddc1e14
MrBrambleberry <bamburyn@gmail.com>
2020-08-31 17:10:39 +0100

    [NB] Extract character thumnail to its own component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React from "react";
+import { CharacterThumbnail } from './components/CharacterThumbnail'
 
 function App() {
   return (
     <div>
-      <div>
-        <h2>Iron Man</h2>
-        <p>Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man.</p>
-        <img src="http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55/portrait_incredible.jpg" />
-      </div>
-
-      <span>"Data provided by Marvel. © 2014 Marvel"</span>
+      <h1>Comic book app</h1>
+      <CharacterThumbnail />
+      <span>Data provided by Marvel. © 2014 Marvel</span>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Comic book app/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('On the main page', () => {
+  it("it renders static content", async () => {
+    await render(<App />);
+    expect(screen.getByText("Comic book app")).toBeInTheDocument();
+    expect(screen.getByText("Iron Man")).toBeInTheDocument();
+  });
+})
+
